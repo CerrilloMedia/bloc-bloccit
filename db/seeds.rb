cb38cedd5ce3a84fbc6eb4db9c1205a57440f269
Armando Cerrillo <armando@cerrillomedia.org>
2017-03-08 05:00:42 +0000

    create unique posts & comments in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,24 @@ require 'random_data'
 #Create Posts
 50.times do
     Post.create!(
-        title:  RandomData.random_sentence,
-        body:   RandomData.random_paragraph
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph
     )
 end
 
+Post.find_or_create_by!(title: "unique post-title", body: "unique post-body")
+
 posts = Post.all
+
 100.times do
-   Comment.create!(
-       post: posts.sample,
-       body: RandomData.random_paragraph
+    Comment.create!(
+        post: posts.sample,
+        body: RandomData.random_paragraph
    )
 end
+
+Comment.find_or_create_by!(post: posts.find_by(title: "unique post-title"), body: "unique comment-body")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-
-
