8f8c3147c52ac5d59105644ddaa3f3f4d01a4cee
Armando Cerrillo <armando@cerrillomedia.org>
2017-03-10 07:38:36 +0000

    create method to alter every 5th post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,25 @@
+require 'time'
+
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
   end
 
   def show
+    
   end
 
   def new
+    
+    Post.all.each do | post |
+      # when creating a new post the individual post should be sent to get checked before being added to the DB.
+      # mimic passing a post to check for spam
+      post.check_post_for_spam
+    end
   end
 
   def edit
+    
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ActiveRecord::Base
     has_many :comments
+    
+    def check_post_for_spam
+        # check if it's id is either the 1st or 5th and alter it's title with "SPAM"
+        if (self.id % 5 === 0)
+            self.title = "SPAM"
+            puts "SPAM FOUND: post #" + self.id.to_s + " has been flagged."
+        end
+        #save to DB
+        self.save
+    end
+    
+    
+    
 end
