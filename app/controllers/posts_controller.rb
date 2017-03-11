78624c38b3187e51cb70f49555706ff66b3aa27f
Armando Cerrillo <armando@cerrillomedia.org>
2017-03-11 20:24:26 +0000

    create, test and view new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,33 @@
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
   end
 
   def show
+      @post = Post.find(params[:id])
   end
 
   def new
+    # the new.html view needs this info when setting up the form prior to sending it to the create method which posts it to the DB
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    
+    if @post.save
+      flash[:notice] = "Post was saved"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
   end
+  
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,26 +18,59 @@ RSpec.describe PostsController, type: :controller do
     
   end
   
-
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get :edit
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+    
+    it "instantiates @post" do
+      get :new
+      # assigns() gives us access to the @post variable, assigning it to :post
+      expect(assigns(:post)).not_to be_nil
+    end
+    
+  end
+  
+  describe "POST create" do # POST(verb) for HTTP requests, not the name Post from database table
+    it "increases the number of Post by 1" do
+      # post :create writes to the database
+      expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post, :count).by(1)
+    end
+    
+    it "assigns the new post to @post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      # expect newly created post is equal to the last one in the Post table in db
+      expect(assigns(:post)).to eq Post.last
+    end
+    
+    it "redirects to the new post" do
+      post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      # expect to redirect to most recent posts view
+      expect(response).to redirect_to Post.last
+    end
+  end
+  
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_post.id }
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #show view" do
+      get :show, {id: my_post.id}
+      expect(response).to render_template :show
+    end
+    
+    it "assigns my_post to @post" do
+      get :show, {id: my_post.id }
+      expect(assigns(:post)).to eq(my_post)
+    end
+  end
+    
 end
