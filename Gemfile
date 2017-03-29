d34f7caab11cdfbfcfe08e8de8b383df5f279ee4
Armando Cerrillo <armando@cerrillomedia.org>
2017-03-29 20:08:58 +0000

    generate User model and add validations & tests

--- Gemfile
+++ Gemfile
@@ -32,3 +32,5 @@ source 'https://rubygems.org'
  gem 'turbolinks'
  # bootstrap-sass
  gem 'bootstrap-sass'
+ # Used for encrypting User passwords
+ gem 'bcrypt'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :posts, except: [:index] # children. #index is no longer an available view since we now list them in the parents #show view
   end
   
+  resources :users, only: [:new, :create]
+  
   get 'about' => "welcome#about"
   
   root 'welcome#index'
